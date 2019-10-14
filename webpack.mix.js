764e1425295b4c0e044720000ca85eaa7daba565
ander7agar <ander7agar@gmail.com>
2019-10-14 15:09:41 +0200

    webpack config

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,17 @@
 const mix = require('laravel-mix');
+const fs = require('fs');
+
+const JS_FILES_DIR = 'resources/js/';
+const JS_OUTPUT_DIR = 'public/js/';
+const JS_EXCLUDE_FILES = [
+    'required', 'components'
+];
+
+const SASS_FILES_DIR = 'resources/sass/';
+const SASS_OUTPUT_DIR = 'public/css/';
+const SASS_EXCLUDE_FILES = [
+    'imported'
+];
 
 /*
  |--------------------------------------------------------------------------
@@ -11,5 +24,24 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+const JS_FILES = fs.readdirSync(JS_FILES_DIR);
+
+JS_EXCLUDE_FILES.forEach(function (value) {
+    JS_FILES.splice(JS_FILES.indexOf(value), 1);
+});
+
+const SASS_FILES = fs.readdirSync(SASS_FILES_DIR);
+
+SASS_EXCLUDE_FILES.forEach(function (value) {
+    SASS_FILES.splice(SASS_FILES.indexOf(value), 1);
+});
+
+JS_FILES.forEach(function (value, index, array) {
+    mix.js(JS_FILES_DIR + value, JS_OUTPUT_DIR);
+});
+
+SASS_FILES.forEach(function (value, index, array) {
+    mix.sass(SASS_FILES_DIR + value, SASS_OUTPUT_DIR);
+});
+
+mix.disableSuccessNotifications();
