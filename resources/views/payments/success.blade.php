bdf811f3b48e46bf11f70562fdd65520a6b0bf18
ander7agar <ander7agar@gmail.com>
2019-11-08 19:51:36 +0100

    update success page

--- resources/views/payments/success.blade.php
+++ resources/views/payments/success.blade.php
@@ -1,23 +1,23 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
+    <div class="container p-congratulations vh-80">
         <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">Success!!</div>
-
-                    <div class="card-body">
-                        @if (session('status'))
-                            <div class="alert alert-success" role="alert">
-                                {{ session('status') }}
-                            </div>
-                        @endif
-
-                        Your payment was successful. We will send {{ $payment->to_send / 1000 }} {{ strtoupper($payment->crypto) }} to {{ '@' . $payment->send_to }}.
-                    </div>
-                </div>
+            <div class="col-12 text-center mt-5">
+                <img src="{{ asset('img/congratulations.png') }}" alt="" class="img-fluid">
+                <ul class="list-unstyled">
+                    <li class="mt-5"><h1 class="font-36 c-primary font-weight-bold">¡El cometa ha sido enviado!</h1></li>
+                    <li><p class="mb-0 font-16">Enhorabuena, el proceso de compra se ha completado con éxito.</p></li>
+                    <li><p class="font-16">En breves momentos recibirás un cometa con tu saldo.</p></li>
+                </ul>
             </div>
+
+            <div class="col-12 col-md-4 text-center mb-5">
+                <ul class="list-unstyled">
+                    <li><p class="font-10">Cualquier duda o problema relacionado con esta operación ponte en contacto con nuestro equipo de soporte en support@onecomet.co</p></li>
+                </ul>
+            </div>
+
         </div>
     </div>
 @endsection
