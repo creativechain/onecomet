b6b269aa9becdf4a2c3dccb8fc479c04cbf12470
ander7agar <ander7agar@gmail.com>
2019-11-08 19:58:16 +0100

    update rejected and error page

--- resources/views/payments/error.blade.php
+++ resources/views/payments/error.blade.php
@@ -1,22 +1,22 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
+    <div class="container p-congratulations vh-80">
         <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">Error</div>
-
-                    <div class="card-body">
-                        @if (session('status'))
-                            <div class="alert alert-success" role="alert">
-                                {{ session('status') }}
-                            </div>
-                        @endif
-                            Your payment has not been completed correctly. Try again or contact our support..
-                    </div>
-                </div>
+            <div class="col-12 text-center mt-5">
+                <img src="{{ asset('img/congratulations.png') }}" alt="" class="img-fluid">
+                <ul class="list-unstyled">
+                    <li class="mt-5"><h1 class="font-36 c-primary font-weight-bold">¡Upss! Algo ha ido mal.</h1></li>
+                    <li><p class="mb-0 font-16">No se ha podido detectar el pago.</p></li>
+                </ul>
             </div>
+
+            <div class="col-12 col-md-4 text-center mb-5">
+                <ul class="list-unstyled">
+                    <li><p class="font-10">Cualquier duda o problema relacionado con esta operación ponte en contacto con nuestro equipo de soporte en support@onecomet.co</p></li>
+                </ul>
+            </div>
+
         </div>
     </div>
 @endsection
