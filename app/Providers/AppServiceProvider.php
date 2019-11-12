444bcd5a7bb10cb424bd1e24b78ed3890b209d8f
ander7agar <ander7agar@gmail.com>
2019-11-12 16:34:21 +0100

    fix mysql string length

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 use Stripe\Stripe;
 
@@ -25,6 +26,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+        Schema::defaultStringLength(191);
         Stripe::setApiKey(env('STRIPE_SECRET_KEY'));
     }
 }
