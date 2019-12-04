8413d38c5c34b1fbfbf7a2b912a1cf56920258d3
ander7agar <ander7agar@gmail.com>
2019-12-04 18:28:31 +0100

    change error image

--- resources/views/payments/rejected.blade.php
+++ resources/views/payments/rejected.blade.php
@@ -4,7 +4,7 @@
     <div class="container p-congratulations vh-80">
         <div class="row justify-content-center">
             <div class="col-12 text-center mt-5">
-                <img src="{{ asset('img/congratulations.png') }}" alt="" class="img-fluid">
+                <img src="{{ asset('img/onecomet-opps.png') }}" alt="" class="img-fluid">
                 <ul class="list-unstyled">
                     <li class="mt-5"><h1 class="font-36 c-primary font-weight-bold">{{ __('web.rejected_process.title') }}</h1></li>
                     <li><p class="mb-0 font-16">{{ __('web.rejected_process.description') }}</p></li>
