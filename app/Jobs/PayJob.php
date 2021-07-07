da2ed4af0b7849fc4e0e6e9034d5956a8f26af5d
ander7agar <ander7agar@gmail.com>
2021-07-07 15:25:13 +0200

    Fix double payments

--- app/Jobs/PayJob.php
+++ app/Jobs/PayJob.php
@@ -30,7 +30,6 @@ class PayJob implements ShouldQueue
     {
         //
         $this->paymentId = $paymentId;
-        $this->payment = Payment::query()->find($paymentId);
     }
 
     /**
@@ -41,63 +40,69 @@ class PayJob implements ShouldQueue
      */
     public function handle()
     {
-        //
+
+        $this->payment = Payment::query()->find($this->paymentId);
         if ($this->payment) {
-            try {
-                $toSend = $this->payment->formatToSend();
-                $from = env('CREA_SENDER_USER');
-                $to = $this->payment->send_to;
-                $wif = env('CREA_SENDER_KEY');
-                $identifier = $this->payment->identifier;
+            if ($this->payment->status === 'published') {
+                try {
+                    $toSend = $this->payment->formatToSend();
+                    $from = env('CREA_SENDER_USER');
+                    $to = $this->payment->send_to;
+                    $wif = env('CREA_SENDER_KEY');
+                    $identifier = $this->payment->identifier;
 
-                $error = false;
-                $output = [];
-                $nodes = env('OC_NODE_URL');
-                $command = "crea-tx transfer $from $to \"$toSend\" $identifier $wif --node $nodes";
-                Log::debug("Command: $command");
-                exec($command, $output,  $error);
+                    $error = false;
+                    $output = [];
+                    $nodes = env('OC_NODE_URL');
+                    $command = "crea-tx transfer $from $to \"$toSend\" $identifier $wif --node $nodes";
+                    Log::debug("Command: $command");
+                    exec($command, $output,  $error);
 
-                if (!$error) {
-                    $this->payment->status = 'oc_paid';
-                    $this->payment->save();
-
-                    $output = implode(" ", $output);
-                    $txData = json_decode($output, true);
-                    Log::info("Payment sent! $toSend to @$to: Result: $output");
-                    //dd($txData);
-
-                    PaymentMeta::query()->insert(array (
-                        [
-                            'payment_id' => $this->payment->id,
-                            'meta_key' => '_txid',
-                            'meta_value' => $txData['id']
-                        ],
-                        [
-                            'payment_id' => $this->payment->id,
-                            'meta_key' => '_block',
-                            'meta_value' => $txData['block_num']
-                        ],
-                    ));
-
-                } else {
-                    Log::error("Error sending amount. Exit Code " . $error);
-                    if (config('env') !== 'production') {
-                        Log::info('App is not in production environment. Simulating success payment... ');
+                    if (!$error) {
                         $this->payment->status = 'oc_paid';
                         $this->payment->save();
+
+                        $output = implode(" ", $output);
+                        $txData = json_decode($output, true);
+                        Log::info("Payment sent! $toSend to @$to: Result: $output");
+                        //dd($txData);
+
+                        PaymentMeta::query()->insert(array (
+                            [
+                                'payment_id' => $this->payment->id,
+                                'meta_key' => '_txid',
+                                'meta_value' => $txData['id']
+                            ],
+                            [
+                                'payment_id' => $this->payment->id,
+                                'meta_key' => '_block',
+                                'meta_value' => $txData['block_num']
+                            ],
+                        ));
+
+                    } else {
+                        Log::error("Error sending amount. Exit Code " . $error);
+                        if (config('env') !== 'production') {
+                            Log::info('App is not in production environment. Simulating success payment... ');
+                            $this->payment->status = 'oc_paid';
+                            $this->payment->save();
+                        }
                     }
+                } catch (\Exception $e) {
+                    Log::error('Error paying ' . $this->payment, $e->getTrace());
                 }
-            } catch (\Exception $e) {
-                Log::error('Error paying ' . $this->payment, $e->getTrace());
+
+                try {
+                    Log::info('Releasing ' . $this->payment);
+                    TruustOrder::finishPayment($this->payment);
+                    Log::info($this->payment . ' release successfully.');
+                } catch (\Exception $e) {
+                    Log::error('Error releasing ' . $this->payment, $e->getTrace());
+                }
+            } else {
+                Log::error("Error processing payment " . $this->payment . '. Not valid status.');;
             }
 
-            try {
-                Log::info('Releasing ' . $this->payment);
-                TruustOrder::finishPayment($this->payment);
-                Log::info($this->payment . ' release successfully.');
-            } catch (\Exception $e) {
-                Log::error('Error releasing ' . $this->payment, $e->getTrace());
-            }
 
         } else {
             throw new NotFoundResourceException("Payment with ID $this->paymentId not found");
